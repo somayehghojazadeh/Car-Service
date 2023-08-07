c405bf9bec3fc1e08366e7d2b3ffaa6f0cdcb16f
apple <hamedkhademi14@gmail.com>
2023-08-07 14:44:45 +0330

    fix for start

---
+++ cypress/e2e/PWA/Train/Bus.cy.js
@@ -0,0 +1,21 @@
+// PWA Train Test
+import {login} from "../login/Login";
+
+
+
+login()
+context('Actions', () => {
+  beforeEach(() => {
+    cy.viewport('iphone-6')
+  })
+
+
+
+  it('.type() - type into a DOM element', () => {
+
+
+
+
+
+  })
+})
